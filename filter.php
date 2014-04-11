d366c030b9c9e588e30913b7d01d8d6a8deb83e6
Kevin Wiliarty <kevin.wiliarty@gmail.com>
2014-04-10 23:49:23 -0400

    separates char replacement as own static method

--- filter.php
+++ filter.php
@@ -24,13 +24,22 @@ class filter_ipa extends moodle_text_filter {
         ':'        => 'ː'
     );
 
-    public function filter($text, array $options = array()) {
+    public static function ipa_replace_chars($rawtext) {
         $mappings = self::$filteripadefaults;
         $asciis = array_keys($mappings);
         $utf8s = array_values($mappings);
+        $ipatext = str_replace($asciis, $utf8s, $rawtext);
+        return $ipatext;
+    }
+
+    public static function return_ipa_json() {
+        return json_encode(self::$filteripadefaults);
+    }
+
+    public function filter($text, array $options = array()) {
         preg_match_all('|ipa{(.*?)}|', $text, $ipas);
         foreach ($ipas[1] as $key => $markup) {
-            $display = str_replace($asciis, $utf8s, $markup);
+            $display = self::ipa_replace_chars($markup);
             $span = html_writer::tag('span', $display, array('class'=>'filter-ipa'));
             $text = str_replace($ipas[0][$key], $span, $text);
         }
