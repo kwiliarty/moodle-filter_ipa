13918eb0bc2c4506f80ac0f001bd560cebe2c54d
Kevin Wiliarty <kevin.wiliarty@gmail.com>
2014-05-14 23:04:14 -0400

    adds $plugin->release, MATURITY_STABLE and version bump

--- version.php
+++ version.php
@@ -27,7 +27,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2014050600;
+$plugin->version   = 2014051400;
 $plugin->requires  = 2013111800;
+$plugin->release   = 'v1.0-M26 (Build: 2014051400)';
 $plugin->component = 'filter_ipa';
-$plugin->maturity  = MATURITY_RC;
+$plugin->maturity  = MATURITY_STABLE;
