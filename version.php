13de87f0d4da5ed8401039dde5d7d563afbfc7a2
Kevin Wiliarty <kevin.wiliarty@gmail.com>
2015-11-28 00:02:16 -0500

    bumps version to v1.1.2-master

--- version.php
+++ version.php
@@ -27,7 +27,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2015102600;
+$plugin->version   = 2015112800;
 $plugin->requires  = 2014051200;
 $plugin->release   = 'v1.1.2-dev (Build: 2015102600)';
 $plugin->component = 'filter_ipa';
