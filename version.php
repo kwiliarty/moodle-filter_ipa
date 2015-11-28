71e767657584f6032216795377b6b34fa5b4a5c2
Kevin Wiliarty <kevin.wiliarty@gmail.com>
2015-11-28 00:17:41 -0500

    releases v1.1.2-M30

--- version.php
+++ version.php
@@ -27,8 +27,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2015102600;
-$plugin->requires  = 2014051200;
-$plugin->release   = 'v1.1.2-dev (Build: 2015102600)';
+$plugin->version   = 2015112700;
+$plugin->requires  = 2015111600;
+$plugin->release   = 'v1.1.2-M30 (Build: 2015112700)';
 $plugin->component = 'filter_ipa';
-$plugin->maturity  = MATURITY_ALPHA;
+$plugin->maturity  = MATURITY_STABLE;
